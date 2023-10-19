3cbd3267e072e1e5f09f28fb5a6b6f0aeb0c71d3
jawalesarkar <145426116+jawalesarkar@users.noreply.github.com>
2023-10-19 08:37:45 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'newdesign';
+  fun(){
+    alert("You pressed the button")
+  }
+}
